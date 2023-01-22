d688c72a425769f326de87517ec7fbdacd810e97
cesarbouzas <cesarbouzaspaw@gmail.com>
2023-01-22 20:07:27 +0100

    Update contacto.service.ts

--- 004_FrontEnd_Angular/tunelapp/src/app/shared/services/contacto.service.ts
+++ 004_FrontEnd_Angular/tunelapp/src/app/shared/services/contacto.service.ts
@@ -12,10 +12,16 @@ export class ContactoService {
   constructor(private httpClient : HttpClient) { }
 
   public recuperarContacto():Observable<any>{
-    const auth=btoa('demo:demouser');
-    const headers=new HttpHeaders().set('Authorization', `Basic ${auth}`);
 
-    return this.httpClient.get<any>('http://localhost:33333/contact/contact?columns=cto_id,cat_id,cto_company,cto_contact_name,cto_phone,cto_email,cto_observations',{headers});
+    var headers_object = new HttpHeaders();
+    headers_object.append('Content-Type', 'application/json');
+    headers_object.append("Authorization", "Basic " + btoa("demo:demouser"));
+
+    const httpOptions = {
+      headers: headers_object
+    };
+
+    return this.httpClient.get<any>('http://localhost:33333/contact/contact?columns=cto_id,cat_id,cto_company,cto_contact_name,cto_phone,cto_email,cto_observations',httpOptions);
 }
 }
 
